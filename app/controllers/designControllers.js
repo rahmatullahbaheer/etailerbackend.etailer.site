28bd865b791649820ee3ffe8dee429be63b04943
rahmatullahbaheer <baheer224@gmail.com>
2025-05-21 09:32:25 +0500

    work

--- app/controllers/designControllers.js
+++ app/controllers/designControllers.js
@@ -68,7 +68,7 @@ class designControllers {
       );
       if (rows.length === 0)
         return res.status(404).json({ message: "Design not found" });
-      return res.json(rows[0]);
+      return res.status(200).json({ data: rows[0] });
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
